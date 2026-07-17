fda94a0d53cf411640cc70595270235d402cce3b
zoinkgamergd-create <zoinkgamergd@gmail.com>
2026-07-16 22:47:31 -0300

    sayo

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/GJBaseGameLayer.hpp>
 
 using namespace geode::prelude;
 
+class $modify(MyBaseGameLayer, GJBaseGameLayer) {
+    void pushButton(PlayerButton btn, bool isPlayer2) {
+        GJBaseGameLayer::pushButton(btn, isPlayer2);
+        // Aquí irá tu lógica de animaciones una vez que confirmemos que compila
+    }
+};
